e8fb3d5309f5b4c0193845696c19e5d83fd036ab
Saheen Alam Shuvo <saheenshuvo182@gmail.com>
2025-05-21 23:04:37 +0600

    Add new user into db

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -4,7 +4,7 @@ import { useNavigate } from "react-router-dom";
 import AuthContext from "../context/AuthContext/AuthContext";
 
 const axiosSecure = axios.create({
-  baseURL: "",
+  baseURL: "http://localhost:5000",
 });
 const useAxiosSecure = () => {
   const navigate = useNavigate();
